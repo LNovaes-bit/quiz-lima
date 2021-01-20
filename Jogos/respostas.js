df6424644fba1e92af6b7c4414405cbd8fd94b74
LNovaes-bit <72613980+LNovaes-bit@users.noreply.github.com>
2021-01-20 14:43:13 -0300

    Add files via upload

---
+++ Jogos/respostas.js
@@ -0,0 +1,195 @@
+function pergunta1(){
+let alt11 = document.getElementById("alt11").checked
+let resultado = "Acertou!"
+let resultado2 = "Errou, tente novamente!"
+
+if (alt11 == true) {
+    document.getElementById("resp1").textContent = resultado
+    
+}
+
+else {
+    document.getElementById("resp1").textContent = resultado2
+}
+
+}
+
+function pergunta2(){
+    let alt22 = document.getElementById("alt22").checked
+    let resultado = "Acertou!"
+    let resultado2 = "Errou, tente novamente!"
+    
+    if (alt22 == true) {
+        document.getElementById("resp2").textContent = resultado
+        
+    }
+    
+    else {
+        document.getElementById("resp2").textContent = resultado2
+    }
+    
+    }
+
+    function pergunta3(){
+        let alt32 = document.getElementById("alt32").checked
+        let resultado = "Acertou!"
+        let resultado2 = "Errou, tente novamente!"
+        
+        if (alt32 == true) {
+            document.getElementById("resp3").textContent = resultado
+            
+        }
+        
+        else {
+            document.getElementById("resp3").textContent = resultado2
+        }
+        
+        }
+
+        function pergunta4(){
+            let alt43 = document.getElementById("alt43").checked
+            let resultado = "Acertou!"
+            let resultado2 = "Errou, tente novamente!"
+            
+            if (alt43 == true) {
+                document.getElementById("resp4").textContent = resultado
+                
+            }
+            
+            else {
+                document.getElementById("resp4").textContent = resultado2
+            }
+            
+            }
+    
+            function pergunta5(){
+                let alt53 = document.getElementById("alt53").checked
+                let resultado = "Acertou!"
+                let resultado2 = "Errou, tente novamente!"
+                
+                if (alt53 == true) {
+                    document.getElementById("resp5").textContent = resultado
+                    
+                }
+                
+                else {
+                    document.getElementById("resp5").textContent = resultado2
+                }
+                
+                }
+        
+                function pergunta6(){
+                    let alt61 = document.getElementById("alt61").checked
+                    let resultado = "Acertou!"
+                    let resultado2 = "Errou, tente novamente!"
+                    
+                    if (alt61 == true) {
+                        document.getElementById("resp6").textContent = resultado
+                        
+                    }
+                    
+                    else {
+                        document.getElementById("resp6").textContent = resultado2
+                    }
+                    
+                    }
+            
+                    function pergunta7(){
+                        let alt73 = document.getElementById("alt73").checked
+                        let resultado = "Acertou!"
+                        let resultado2 = "Errou, tente novamente!"
+                        
+                        if (alt73 == true) {
+                            document.getElementById("resp7").textContent = resultado
+                            
+                        }
+                        
+                        else {
+                            document.getElementById("resp7").textContent = resultado2
+                        }
+                        
+                        }
+                
+                        function pergunta8(){
+                            let alt82 = document.getElementById("alt82").checked
+                            let resultado = "Acertou!"
+                            let resultado2 = "Errou, tente novamente!"
+                            
+                            if (alt82 == true) {
+                                document.getElementById("resp8").textContent = resultado
+                                
+                            }
+                            
+                            else {
+                                document.getElementById("resp8").textContent = resultado2
+                            }
+                            
+                            }
+                    
+                            function pergunta9(){
+                                let alt92 = document.getElementById("alt92").checked
+                                let resultado = "Acertou!"
+                                let resultado2 = "Errou, tente novamente!"
+                                
+                                if (alt92 == true) {
+                                    document.getElementById("resp9").textContent = resultado
+                                    
+                                }
+                                
+                                else {
+                                    document.getElementById("resp9").textContent = resultado2
+                                }
+                                
+                                }
+                        
+                                function pergunta10(){
+                                    let alt101 = document.getElementById("alt101").checked
+                                    let resultado = "Acertou!"
+                                    let resultado2 = "Errou, tente novamente!"
+                                    
+                                    if (alt101 == true) {
+                                        document.getElementById("resp10").textContent = resultado
+                                        
+                                    }
+                                    
+                                    else {
+                                        document.getElementById("resp10").textContent = resultado2
+                                    }
+                                    
+                                    }
+                            
+                                    function pergunta11(){
+                                        let alt113 = document.getElementById("alt113").checked
+                                        let resultado = "Acertou!"
+                                        let resultado2 = "Errou, tente novamente!"
+                                        
+                                        if (alt113 == true) {
+                                            document.getElementById("resp11").textContent = resultado
+                                            
+                                        }
+                                        
+                                        else {
+                                            document.getElementById("resp11").textContent = resultado2
+                                        }
+                                        
+                                        }
+                                
+                                        function pergunta12(){
+                                            let alt122 = document.getElementById("alt122").checked
+                                            let resultado = "Acertou!"
+                                            let resultado2 = "Errou, tente novamente!"
+                                            
+                                            if (alt122 == true) {
+                                                document.getElementById("resp12").textContent = resultado
+                                                
+                                            }
+                                            
+                                            else {
+                                                document.getElementById("resp12").textContent = resultado2
+                                            }
+                                            
+                                            }
+                                    
+                                                                                                                                    
+        
+    
